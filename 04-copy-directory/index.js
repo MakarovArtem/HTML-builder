8da4122f03dc27d1b883f2fd8a4be0a693142ed6
Artem <artemmakarov76@yandex.ru>
2022-11-07 23:23:03 +0300

    init: start task #4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs');
+const path = require("path");
 
+const fileDir = path.dirname(__filename);//путь к директории с текущим файлом
+const from = path.resolve(fileDir, "files");//исходная папка
+const to = path.resolve(fileDir, "files-copy");//целевая папка
+
+fs.readdir(to, (err, items) => {
+  if (err) {
+    console.log(err.code);
+
+    fs.mkdir(to, err => {
+      console.log(`\nПапка для копирования файлов была успешно создана:\n\n`);
+    });
+  } else {
+    // console.log(`\nФайлы в папке "${to}" прочитаны удачно\n`);
+
+    for (let i=0; i<items.length; i++) {
+      let fileCurrent = items[i];
+      const dest = path.resolve(to, fileCurrent);
+
+      fs.rm(dest, (err) => {
+        // console.log(`Файл "${fileCurrent}" был удален\n`);
+        console.log("Удаляю");
+      });
+    }
+  }
+});
+
+fs.readdir(from, (err, items) => {
+
+  for (let i=0; i<items.length; i++) {
+    let fileCurrent = items[i];
+    const src = path.resolve(from, fileCurrent);
+    const dest = path.resolve(to, fileCurrent);
+
+    fs.copyFile(src, dest, (err) => {
+      // console.log(`\nФайл "${fileCurrent}" был успешно скопирован из этой папки >>> ${from}\nв эту папку >>> ${to}\n`);
+      console.log("Копирую");
+    });
+  }
+});
